feaeb4e95097c825cb6106df2d32c65d847237a2
Mojgan Farmahini <Mojgan360@gmail.com>
2019-02-07 11:50:48 +0100

    Updates

---
+++ precache-manifest.7f461cc29e1547c0472f9e2cfd3fcb08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2a9c315f8a762380f2d",
+    "url": "/react-simple-contact-manager/static/css/main.a15c30df.chunk.css"
+  },
+  {
+    "revision": "f2a9c315f8a762380f2d",
+    "url": "/react-simple-contact-manager/static/js/main.f2a9c315.chunk.js"
+  },
+  {
+    "revision": "e9cafee35c4b3a62c48b",
+    "url": "/react-simple-contact-manager/static/css/1.b2bc0793.chunk.css"
+  },
+  {
+    "revision": "e9cafee35c4b3a62c48b",
+    "url": "/react-simple-contact-manager/static/js/1.e9cafee3.chunk.js"
+  },
+  {
+    "revision": "bb3cf9ed5343685f70bf",
+    "url": "/react-simple-contact-manager/static/js/runtime~main.bb3cf9ed.js"
+  },
+  {
+    "revision": "a7b928a9322e519a35eac8711aaa60b9",
+    "url": "/react-simple-contact-manager/index.html"
+  }
+];
